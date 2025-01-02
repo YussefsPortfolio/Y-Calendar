b2abd4918f4cd1def71355bfa4276ad66be04c3d
Yussef888 <115888298+iLoveMyPortfolio@users.noreply.github.com>
2025-01-02 08:59:46 +0100

    Initial Commit

---
+++ Y-CalendarTests/Y_CalendarTests.swift
@@ -0,0 +1,17 @@
+//
+//  Y_CalendarTests.swift
+//  Y-CalendarTests
+//
+//  Created by Yussef Arsalan on 02.01.25.
+//
+
+import Testing
+@testable import Y_Calendar
+
+struct Y_CalendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
